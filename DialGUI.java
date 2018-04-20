c167f8d31039c4f391ba285aa4d4bd7aabbb24d9
BingwenCheng <36852245+BingwenCheng@users.noreply.github.com>
2018-04-20 10:32:15 +1000

    Add files via upload

---
+++ DialGUI.java
@@ -0,0 +1,85 @@
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.GridLayout;
+
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+
+public class DialGUI implements Runnable,MyObserver {
+
+	private static final Dimension COLORDIM = new Dimension(
+			Dial.dim.height * 3, Dial.dim.height * 3);
+	/**
+	 * DialGUI - this application enables the users to change the color of a
+	 * rectangle by altering 3 dials. The dial alter the RGB components of the
+	 * color. Eric McCreath 2015,2017 - GPL
+	 */
+
+	JFrame jframe;
+	JComponent colorComp;
+	Dial dialr = new Dial(this);
+	Dial dialg = new Dial(this);
+	Dial dialb = new Dial(this);
+
+
+	public DialGUI() {
+		SwingUtilities.invokeLater(this);
+
+		dialr.registerObserver(this);
+//		dialr.notifyObservers();
+		dialg.registerObserver(this);
+//		dialg.notifyObservers();
+		dialb.registerObserver(this);
+//		dialb.notifyObservers();
+
+	}
+
+	public static void main(String[] args) {
+		new DialGUI();
+	}
+
+	// start up the JFrame add the dials and components 
+	public void run() {
+		jframe = new JFrame("DialGUI");
+		jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//		dialr = new Dial(this);
+//		dialg = new Dial(this);
+//		dialb = new Dial(this);
+		dialr.color = Color.red;
+		dialg.color = Color.green;
+		dialb.color = Color.blue;
+
+		colorComp = new JPanel() {
+			protected void paintComponent(Graphics g) {
+				g.setColor(this.getForeground());
+				g.fillRect(0, 0, COLORDIM.width, COLORDIM.height);
+			}
+		};
+		colorComp.setPreferredSize(COLORDIM);
+		JPanel dialpanel = new JPanel();
+		dialpanel.setLayout(new GridLayout(3, 1));
+		dialpanel.add(dialr);
+		dialpanel.add(dialg);
+		dialpanel.add(dialb);
+		jframe.getContentPane().add(dialpanel, BorderLayout.EAST);
+		jframe.getContentPane().add(colorComp, BorderLayout.CENTER);
+
+		update();
+		jframe.setVisible(true);
+		jframe.pack();
+	}
+
+	// update - get the values from the dials and set the color of 
+	//          the panel based on these values.   
+	public void update() {
+		float r = (float) dialr.value();
+		float g = (float) dialg.value();
+		float b = (float) dialb.value();
+		colorComp.setForeground(new Color(r, g, b));
+		colorComp.repaint();
+	}
+}
